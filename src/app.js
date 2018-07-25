ca9135a1153fbd12288289234dd530ad91a8fc9e
Jason Snellbaker <jsnellbaker@appnexus.com>
2018-07-25 15:15:04 -0400

    add output dir, update endpoint names, add support for raw endpoint

--- src/app.js
+++ src/app.js
@@ -4,13 +4,17 @@ const app = express();
 const bodyParser = require('body-parser');
 const csvWriter = require('csv-write-stream');
 const fs = require('file-system');
-const fileName = 'out.csv';
+const csvFileName = 'output/out.csv';
+const rawFileName = 'output/out.log';
+const headers = ['timestamp', 'userId', 'bookId'];
 const port = 3000;
 
 function startup() {
   const writer = csvWriter({sendHeaders: false});
-  writer.pipe(fs.createWriteStream(fileName, {flags: 'a'}));
-  writer.write({'timestamp':'timestamp','userId': 'userId', 'bookId': 'bookId'});
+  writer.pipe(fs.createWriteStream(csvFileName, {flags: 'a'}));
+  let headerLine = {};
+  headers.forEach(item => headerLine[item] = item);
+  writer.write(headerLine);
   writer.end();
 }
 
@@ -18,14 +22,21 @@ function timeIs(){
   return Math.floor(Date.now() / 1000);
 }
 
-function writeDataToCsv(rawData) {
+function writeDataToFile(type, rawData) {
   if (Object.keys(rawData).length > 0) { 
     const logData = Object.assign({timestamp: timeIs()}, rawData);
 
-    const writer = csvWriter({sendHeaders: false});
-    writer.pipe(fs.createWriteStream(fileName, {flags: 'a'}));
-    writer.write(logData);
-    writer.end();
+    if (type === 'csv') {
+      const writer = csvWriter({sendHeaders: false});
+      writer.pipe(fs.createWriteStream(csvFileName, {flags: 'a'}));
+      writer.write(logData);
+      writer.end();
+    } else if (type === 'raw') {
+      let logDataStr = JSON.stringify(logData) + "\n";
+      fs.appendFile(rawFileName, logDataStr, (err) => {
+        if (err) throw err;
+      });
+    }
   } else {
     console.log('detected empty body...did not write to file');
   }
@@ -34,16 +45,22 @@ function writeDataToCsv(rawData) {
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.post('/endpoint', function (req, res) {
-  writeDataToCsv(req.body);
+app.post('/csv', function (req, res) {
+  writeDataToFile('csv', req.body);
 
   res.json(req.body);
 });
 
-app.get('/endpoint', function (req, res) {
-  writeDataToCsv(req.query);
+app.get('/csv', function (req, res) {
+  writeDataToFile('csv', req.query);
 
   res.send('Finished writing data to csv');
+});
+
+app.post('/raw', function (req, res) {
+  writeDataToFile('raw', req.body);
+
+  res.json(req.body);
 });
 
 app.listen(port, () => console.log('Listening on port '+ port));
